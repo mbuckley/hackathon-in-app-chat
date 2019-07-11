fe10cc2ffb4d2c450a38dda69f9da7a5e554d63a
Craig Carlyle <craig.carlyle@clio.com>
2019-07-11 10:22:35 -0700

    Comments out chat container, adds reactdom

--- src/containers/Chat.ts
+++ src/containers/Chat.ts
@@ -11,16 +11,24 @@ import PubNubReact from 'pubnub-react';
 
 // import networkErrorImg from '../styles/networkError.png';
 
-export default class extends Component {
+class ChatComponent {
+  public uuid: any;
+  public designation: any;
+  public userProfile: any;
+  public pubnub: any;
+  public state: any;
+}
+
+export default class extends ChatComponent {
   constructor(props) {
     super(props);
     const randomUser = this.getRandomUser();
-    this.uuid = randomUser.uuid;
-    this.designation = randomUser.designation;
-    this.userProfile = {
-      name: randomUser.firstName + ' ' +  randomUser.lastName,
-      image: randomUser.profileImage.lgImage
-    };
+    // this.uuid = randomUser.uuid;
+    // this.designation = randomUser.designation;
+    // this.userProfile = {
+    //   name: randomUser.firstName + ' ' +  randomUser.lastName,
+    //   image: randomUser.profileImage.lgImage
+    // };
     this.pubnub = new PubNubReact({
       publishKey: "pub-c-2c10eb4d-5066-4241-99f9-d82430455cf9",
       subscribeKey: "sub-c-a6263802-9dd9-11e9-8df4-32dd89bcc96f",
@@ -45,238 +53,238 @@ export default class extends Component {
 
   // tag::CHT-2.1[]
   getRandomUser = () => {
-    return users[Math.floor(Math.random() * users.length)];
+    // return users[Math.floor(Math.random() * users.length)];
   };
   // end::CHT-2.1[]
 
   // tag::CHT-4[]
   componentWillMount() {
-    const networkError = new Image();
-    networkError.src = networkErrorImg;
-    this.setState({networkErrorImg: networkError});
+    // const networkError = new Image();
+    // networkError.src = networkErrorImg;
+    // this.setState({networkErrorImg: networkError});
 
-    this.subscribe();
+    // this.subscribe();
 
-    this.pubnub.getPresence(forestChatChannel, (presence) => {
-      if (presence.action === 'join') {
-        let users = this.state.onlineUsers;
+    // this.pubnub.getPresence(forestChatChannel, (presence) => {
+    //   if (presence.action === 'join') {
+    //     let users = this.state.onlineUsers;
 
-        users.push({
-          state: presence.state,
-          uuid: presence.uuid
-        });
+    //     users.push({
+    //       state: presence.state,
+    //       uuid: presence.uuid
+    //     });
 
-        this.setState({
-          onlineUsers: users,
-          onlineUsersCount: this.state.onlineUsersCount + 1
-        });
-      }
+    //     this.setState({
+    //       onlineUsers: users,
+    //       onlineUsersCount: this.state.onlineUsersCount + 1
+    //     });
+    //   }
 
-      if ((presence.action === 'leave') || (presence.action === 'timeout')) {
-        let leftUsers = this.state.onlineUsers.filter(users => users.uuid !== presence.uuid);
+    //   if ((presence.action === 'leave') || (presence.action === 'timeout')) {
+    //     let leftUsers = this.state.onlineUsers.filter(users => users.uuid !== presence.uuid);
 
-        this.setState({
-          onlineUsers: leftUsers,
-        });
+    //     this.setState({
+    //       onlineUsers: leftUsers,
+    //     });
 
-        const length = this.state.onlineUsers.length;
-        this.setState({
-          onlineUsersCount: length
-        });
-      }
+    //     const length = this.state.onlineUsers.length;
+    //     this.setState({
+    //       onlineUsersCount: length
+    //     });
+    //   }
 
-      if (presence.action === 'interval') {
-        if (presence.join || presence.leave || presence.timeout) {
-          let onlineUsers = this.state.onlineUsers;
-          let onlineUsersCount = this.state.onlineUsersCount;
+    //   if (presence.action === 'interval') {
+    //     if (presence.join || presence.leave || presence.timeout) {
+    //       let onlineUsers = this.state.onlineUsers;
+    //       let onlineUsersCount = this.state.onlineUsersCount;
 
-          if (presence.join) {
-            presence.join.map(user => (
-              user !== this.uuid &&
-              onlineUsers.push({
-                state: presence.state,
-                uuid: user
-              })
-            ));
+    //       if (presence.join) {
+    //         presence.join.map(user => (
+    //           user !== this.uuid &&
+    //           onlineUsers.push({
+    //             state: presence.state,
+    //             uuid: user
+    //           })
+    //         ));
 
-            onlineUsersCount += presence.join.length;
-          }
+    //         onlineUsersCount += presence.join.length;
+    //       }
 
-          if (presence.leave) {
-            presence.leave.map(leftUser => onlineUsers.splice(onlineUsers.indexOf(leftUser), 1));
-            onlineUsersCount -= presence.leave.length;
-          }
+    //       if (presence.leave) {
+    //         presence.leave.map(leftUser => onlineUsers.splice(onlineUsers.indexOf(leftUser), 1));
+    //         onlineUsersCount -= presence.leave.length;
+    //       }
 
-          if (presence.timeout) {
-            presence.timeout.map(timeoutUser => onlineUsers.splice(onlineUsers.indexOf(timeoutUser), 1));
-            onlineUsersCount -= presence.timeout.length;
-          }
+    //       if (presence.timeout) {
+    //         presence.timeout.map(timeoutUser => onlineUsers.splice(onlineUsers.indexOf(timeoutUser), 1));
+    //         onlineUsersCount -= presence.timeout.length;
+    //       }
 
-          this.setState({
-            onlineUsers,
-            onlineUsersCount
-          });
-        }
-      }
-    });
+    //       this.setState({
+    //         onlineUsers,
+    //         onlineUsersCount
+    //       });
+    //     }
+    //   }
+    // });
 
-    this.pubnub.getStatus((status) => {
-      if (status.category === 'PNConnectedCategory') {
-        this.hereNow();
+    // this.pubnub.getStatus((status) => {
+    //   if (status.category === 'PNConnectedCategory') {
+    //     this.hereNow();
 
-        this.pubnub.history({
-          channel: forestChatChannel,
-          count:25,
-          reverse: false,
-          stringifiedTimeToken: true
-        }, (status, response) => {
-          const lastMessageWeekday = this.getWeekday(response.endTimeToken);
+    //     this.pubnub.history({
+    //       channel: forestChatChannel,
+    //       count:25,
+    //       reverse: false,
+    //       stringifiedTimeToken: true
+    //     }, (status, response) => {
+    //       const lastMessageWeekday = this.getWeekday(response.endTimeToken);
 
-          this.setState({
-            historyLoaded: true,
-            historyMessages: response.messages,
-            lastMessageWeekday
-          });
+    //       this.setState({
+    //         historyLoaded: true,
+    //         historyMessages: response.messages,
+    //         lastMessageWeekday
+    //       });
 
-          let messageSentDate = this.state.historyMessages.map(message => this.getWeekday(message.timetoken));
-          this.setState({messageSentDate});
-          this.scrollToBottom();
-        });
-      }
+    //       let messageSentDate = this.state.historyMessages.map(message => this.getWeekday(message.timetoken));
+    //       this.setState({messageSentDate});
+    //       this.scrollToBottom();
+    //     });
+    //   }
 
-      if (status.category === 'PNNetworkDownCategory') {
-        this.setState({networkErrorStatus: true});
-      }
+    //   if (status.category === 'PNNetworkDownCategory') {
+    //     this.setState({networkErrorStatus: true});
+    //   }
 
-      if (status.category === 'PNNetworkUpCategory') {
-        this.setState({networkErrorStatus: false});
-        this.pubnub.reconnect();
-        this.scrollToBottom();
-      }
-    });
+    //   if (status.category === 'PNNetworkUpCategory') {
+    //     this.setState({networkErrorStatus: false});
+    //     this.pubnub.reconnect();
+    //     this.scrollToBottom();
+    //   }
+    // });
 
-    this.pubnub.getMessage(forestChatChannel, (m) => {
-      const sendersInfo = this.state.sendersInfo;
+    // this.pubnub.getMessage(forestChatChannel, (m) => {
+    //   const sendersInfo = this.state.sendersInfo;
 
-      sendersInfo.push({
-        senderId: m.message.senderId,
-        text: m.message.text,
-        timetoken: m.timetoken,
-      });
+    //   sendersInfo.push({
+    //     senderId: m.message.senderId,
+    //     text: m.message.text,
+    //     timetoken: m.timetoken,
+    //   });
 
-      this.setState(this.state);
+    //   this.setState(this.state);
 
-      const lastMessageWeekday = this.getWeekday(m.timetoken);
-      this.setState({
-        sendersInfo,
-        lastMessageWeekday
-      });
+    //   const lastMessageWeekday = this.getWeekday(m.timetoken);
+    //   this.setState({
+    //     sendersInfo,
+    //     lastMessageWeekday
+    //   });
 
-      this.scrollToBottom();
-    });
+    //   this.scrollToBottom();
+    // });
 
-    window.addEventListener('beforeunload', this.leaveChat);
+    // window.addEventListener('beforeunload', this.leaveChat);
   }
   // end::CHT-4[]
 
   // tag::CHT-5[]
   componentWillUnmount() {
-    this.leaveChat();
+    // this.leaveChat();
   }
   // end::CHT-5[]
 
   // tag::CHT-3[]
   subscribe = () => {
-    this.pubnub.subscribe({
-      channels: [forestChatChannel],
-      withPresence: true
-    });
+    // this.pubnub.subscribe({
+    //   channels: [forestChatChannel],
+    //   withPresence: true
+    // });
   };
 
   hereNow = () => {
-    this.pubnub.hereNow({
-      channels: [forestChatChannel],
-      includeUUIDs: true,
-      includeState: false
-    }, (status, response) => {
-      this.setState({
-        onlineUsers: response.channels[forestChatChannel].occupants,
-        onlineUsersCount: response.channels[forestChatChannel].occupancy
-      });
-    });
+    // this.pubnub.hereNow({
+    //   channels: [forestChatChannel],
+    //   includeUUIDs: true,
+    //   includeState: false
+    // }, (status, response) => {
+    //   this.setState({
+    //     onlineUsers: response.channels[forestChatChannel].occupants,
+    //     onlineUsersCount: response.channels[forestChatChannel].occupancy
+    //   });
+    // });
   };
 
   leaveChat = () => {
-    this.pubnub.unsubscribeAll();
+    // this.pubnub.unsubscribeAll();
   };
   // end::CHT-3[]
 
   // tag::CHT-2.2[]
   getTime = (timetoken) => {
-    return new Date(parseInt(timetoken.substring(0, 13))).toLocaleTimeString('en-US', { hour: 'numeric', hour12: true, minute: 'numeric' })
+    // return new Date(parseInt(timetoken.substring(0, 13))).toLocaleTimeString('en-US', { hour: 'numeric', hour12: true, minute: 'numeric' })
   };
 
   getDate = (timetoken, messageType, index = 0) => {
-    const messageWeekday = this.getWeekday(timetoken);
-    const date = new Date(parseInt(timetoken.substring(0, 13))).toLocaleDateString('en-US', {day: 'numeric', month: 'long'});
+    // const messageWeekday = this.getWeekday(timetoken);
+    // const date = new Date(parseInt(timetoken.substring(0, 13))).toLocaleDateString('en-US', {day: 'numeric', month: 'long'});
 
-    switch (messageType) {
-      case 'historyMessage':
-        if (this.state.messageSentDate[index - 1] !== messageWeekday) {
-          return `${date}, ${messageWeekday}`;
-        }
+    // switch (messageType) {
+    //   case 'historyMessage':
+    //     if (this.state.messageSentDate[index - 1] !== messageWeekday) {
+    //       return `${date}, ${messageWeekday}`;
+    //     }
 
-        break;
-      case 'senderMessage':
-        if (this.state.lastMessageWeekday !== messageWeekday) {
-          return `${date}, ${messageWeekday}`;
-        }
+    //     break;
+    //   case 'senderMessage':
+    //     if (this.state.lastMessageWeekday !== messageWeekday) {
+    //       return `${date}, ${messageWeekday}`;
+    //     }
 
-        break;
-      default:
-        return;
-    }
+    //     break;
+    //   default:
+    //     return;
+    // }
   };
 
   getWeekday = (timetoken) => {
-    return new Date(parseInt(timetoken.substring(0, 13))).toLocaleDateString('en-US', {weekday: 'long'});
+    // return new Date(parseInt(timetoken.substring(0, 13))).toLocaleDateString('en-US', {weekday: 'long'});
   };
 
 
   getUser = (uuid) => {
-    return users.find( element => element.uuid === uuid);
+    // return users.find( element => element.uuid === uuid);
   };
 
   getUserName = (uuid) => {
-    const user = this.getUser(uuid);
+    // const user = this.getUser(uuid);
 
-    if (user) {
-      return user.firstName + ' ' + user.lastName;
-    }
+    // if (user) {
+    //   return user.firstName + ' ' + user.lastName;
+    // }
   };
 
   getUserDesignation = (uuid) => {
-    const user = this.getUser(uuid);
+    // const user = this.getUser(uuid);
 
-    if (user) {
-      return user.designation;
-    }
+    // if (user) {
+    //   return user.designation;
+    // }
   };
 
   getUserAvatarUrl = (uuid, size) => {
-    const user = this.getUser(uuid);
+    // const user = this.getUser(uuid);
 
-    if (user) {
-        return user.profileImage[size];
-    }
+    // if (user) {
+    //     return user.profileImage[size];
+    // }
   };
 
   scrollToBottom = () => {
-    const elem = document.querySelector(".messageDialog");
+    // const elem = document.querySelector(".messageDialog");
 
-    if(elem) {
-        elem.scrollTop = elem.scrollHeight;
-    }
+    // if(elem) {
+    //     elem.scrollTop = elem.scrollHeight;
+    // }
   };
   // end::CHT-2.2[]
 
@@ -284,32 +292,7 @@ export default class extends Component {
   render() {
     return (
       <div className='grid'>
-        <Header
-          userProfile={this.userProfile}
-          onlineUsersCount={this.state.onlineUsersCount}/>
-        <MessageList
-          uuid={this.uuid}
-          sendersInfo={this.state.sendersInfo}
-          getUserName={this.getUserName}
-          getUserAvatarUrl={this.getUserAvatarUrl}
-          getTime={this.getTime}
-          messageSentDate={this.state.messageSentDate}
-          getDate={this.getDate}
-          historyLoaded={this.state.historyLoaded}
-          historyMessages={this.state.historyMessages}
-          networkErrorStatus={this.state.networkErrorStatus}
-          networkErrorImg={this.state.networkErrorImg}/>
-        <MessageBody
-          uuid={this.uuid}
-          pubnub={this.pubnub}
-          chatName={forestChatChannel}/>
-        <OnlineUsers
-          users={users}
-          getUserAvatarUrl={this.getUserAvatarUrl}
-          loggedInUser={this.uuid}
-          getUserName={this.getUserName}
-          getUserDesignation={this.getUserDesignation}
-          onlineUsers={this.state.onlineUsers}/>
+        <p>foo</p>
         </div>
       );
   }
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
 export * from './components';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as App from './containers/Chat';
+
+ReactDOM.render(<App />, document.getElementById('root'));
